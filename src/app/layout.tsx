fd3f3b9c73d7bbf6c3b28de9cef908a34f6034db
semaxio <kutchms@gmail.com>
2025-08-14 21:57:55 +0300

    fix styles for padding all page, add folder components and lib/feature

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,47 +1,52 @@
-'use client'
+"use client";
 
-import './globals.css'
-import React from 'react'
-import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-import { Header } from 'photo-flow-ui-kit'
+import "./globals.css";
+import React from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { Header } from "photo-flow-ui-kit";
 
 const httpLink = createHttpLink({
-  uri: 'https://inctagram.work/api/v1/graphql',
-})
+  uri: "https://inctagram.work/api/v1/graphql",
+});
 
 const authLink = setContext((_, { headers }) => {
   return {
     headers: {
       ...headers,
-      Authorization: 'Basic YWRtaW5AZ21haWwuY29tOmFkbWlu', // hardcode
+      Authorization: "Basic YWRtaW5AZ21haWwuY29tOmFkbWlu", // hardcode
     },
-  }
-})
+  };
+});
 
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   defaultOptions: {
     query: {
-      fetchPolicy: 'network-only',
+      fetchPolicy: "network-only",
     },
   },
-})
+});
 
 export default function RootLayout({
-                                     children,
-                                   }: Readonly<{
-  children: React.ReactNode
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-    <body>
-    <ApolloProvider client={client}>
-      <Header isSuperAdminPanel isAuth={true} />
-      {children}
-    </ApolloProvider>
-    </body>
+    <html lang="en">
+      <body>
+        <ApolloProvider client={client}>
+          <Header isSuperAdminPanel isAuth={true} />
+          <div className="pt-[60px]">{children}</div>
+        </ApolloProvider>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-'use client'
-import { Typography } from 'photo-flow-ui-kit'
-import './globals.css'
+"use client";
+import { Typography } from "photo-flow-ui-kit";
+import "./globals.css";
 
 export default function Home() {
   return (
-    <div className='pt-[60px]'>
-      <Typography variant={'bold_text_14'}>App page</Typography>
+    <div>
+      <Typography variant={"bold_text_14"}>App page</Typography>
     </div>
-  )
+  );
 }
