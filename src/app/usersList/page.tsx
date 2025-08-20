555d449451040fe32fecdfc1654c370e1ef366b8
den987655 <den987655@gmail.com>
2025-08-20 13:18:02 +0300

    fix: add username

--- src/app/usersList/page.tsx
+++ src/app/usersList/page.tsx
@@ -7,17 +7,17 @@ import FilterIcon from '@/assets/icons/filter.svg'
 import { useQuery } from '@apollo/client'
 
 import { useEffect, useRef, useState } from 'react'
-// import { formatDateToDotFormat } from '@/utils'
 import { twMerge } from 'tailwind-merge'
 import { GET_USERS } from '@/lib/feature/usersList/api/adminApi'
 import { GetUsersResponse, GetUsersVariables } from '@/lib/types/graphql'
 import UserMenu from '@/lib/feature/usersList/ui/UserMenu'
-import { ModalWindow } from 'photo-flow-ui-kit'
+import { ModalWindow, Pagination } from 'photo-flow-ui-kit'
+import { formatDateToDotFormat } from '@/utils'
 
 // import { Pagination } from '@/components/ui/pagination/Pagination'
 
 const headers = [
-  { title: 'UserType ID' },
+  { title: 'User ID' },
   { title: 'Username', icon: <FilterIcon className='mx-auto' /> }, // Без иконки
   { title: 'Profile link' },
   { title: 'Date added', icon: <FilterIcon className='mx-auto' /> },
@@ -56,9 +56,9 @@ export default function ListUsers() {
     setActiveUserId(prev => (prev === userId ? null : userId))
   }
 
-  // const handlePageChange = (newPage: number) => {
-  //   setPageNumber(newPage)
-  // }
+  const handlePageChange = (newPage: number) => {
+    setPageNumber(newPage)
+  }
 
   useEffect(() => {
     console.log('Current page:', pageNumber, 'GraphQL response:', data?.getUsers)
@@ -68,7 +68,6 @@ export default function ListUsers() {
   if (loading) return <div>Loading...</div>
   if (error) return <div>Error: {error.message}</div>
 
-  console.log(activeUserId)
   return (
     <>
       <table onClick={() => setActiveUserId(null)} ref={tableRef} className={'w-full'}>
@@ -91,14 +90,16 @@ export default function ListUsers() {
         <tbody>
           {users.map(el => (
             <tr key={el.id} className={'border-dark-500 text-regular-14 h-[49px] border'}>
-              <td className={'py-[11px] pl-[24px]'}>
-                {el.userBan && <BanIcon className={'h-[24px] w-[24px]'} />}
+              <td className={'flex py-[11px] pl-[60px]'}>
+                {el.userBan && <BanIcon className={'absolute left-68 h-[24px] w-[24px]'} />}
                 {el.id}
               </td>
-              <td className={'py-[11px] pl-[24px]'}>{el.userName}</td>
+              <td className={'py-[12px] pl-[24px]'}>
+                {el.profile.firstName} {el.profile.lastName}
+              </td>
               <td className={'py-[11px] pl-[24px]'}>{el.userName}</td>
               <td className={'py-[11px] pr-[24px] pl-[24px]'}>
-                {/*{formatDateToDotFormat(el.createdAt)}*/}
+                {formatDateToDotFormat(el.createdAt)}
               </td>
               <td className='relative py-[11px] pr-[24px] pl-[24px]'>
                 <div className='relative flex justify-end'>
@@ -122,17 +123,17 @@ export default function ListUsers() {
         </tbody>
       </table>
       <div className='overflow-x-auto'>
-        {/*<Pagination*/}
-        {/*  currentPage={pageNumber}*/}
-        {/*  itemsPerPage={pageSize}*/}
-        {/*  totalCount={data?.getUsers.pagination.totalCount || 0}*/}
-        {/*  pageSize={data?.getUsers.pagination.pagesCount || 1}*/}
-        {/*  onChangePagination={handlePageChange}*/}
-        {/*  // onChangePageSize={size => {*/}
-        {/*  //   setPageSize(size)*/}
-        {/*  //   setPageNumber(1)*/}
-        {/*  // }}*/}
-        {/*/>*/}
+        <Pagination
+          currentPage={pageNumber}
+          itemsPerPage={pageSize}
+          totalCount={data?.getUsers.pagination.totalCount || 0}
+          // pageSize={data?.getUsers.pagination.pagesCount || 1}
+          onChangePagination={handlePageChange}
+          // onChangePageSize={size => {
+          //   setPageSize(size)
+          //   setPageNumber(1)
+          // }}
+        />
       </div>
       <ModalWindow open={isModalOpen} onClose={() => setIsModalOpen(false)} />
     </>
